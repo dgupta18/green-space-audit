de27a72279e743af5ebca48fcc46d641d499dd4a
Divya Gupta <dgupta@Divyas-MacBook-Pro-2.local>
2019-11-14 21:38:58 -0500

    adding user fields & updating home page layout

--- app/src/main/java/com/example/tuanquach/mainactivity/GreenSpace.kt
+++ app/src/main/java/com/example/tuanquach/mainactivity/GreenSpace.kt
@@ -1,11 +1,14 @@
 package com.example.tuanquach.mainactivity
 
-data class GreenSpace (val gsName: String,
-                       val gsLat: Float,
-                       val gsLong: Float,
-                       val gsQuality: String,
-                       val isGSActive: Boolean,
-                       val gsComments: ArrayList<String>)
-
-// TODO: do we want to change comments to a map with the user who commented as the key and the comment as the value?
-// I don't know if kotlin maps have mutable length or not
+data class GreenSpace (
+    val gsName: String,
+    val gsCreator: String,
+    val gsLat: Float,
+    val gsLong: Float,
+    val gsAcres: Float,
+    val gsQuality: String, // low, med, high
+    val gsType: String, // nature-based, people-powered
+    val gsComments: MutableMap<String, String>,
+    val isQuiet: Boolean,
+    val isNearHazards: Boolean
+)
--- app/src/main/java/com/example/tuanquach/mainactivity/User.kt
+++ app/src/main/java/com/example/tuanquach/mainactivity/User.kt
@@ -1,3 +1,13 @@
 package com.example.green_space_audits.mainactivity
 
-data class User (val userName: String ="", val userEmail: String = "", val userPassword: String = "")
+data class User (
+    val userName: String,
+    val userEmail: String,
+    val userPassword: String,
+    val userIsAdmin: Boolean,
+    val uPoints: Int,
+    val uGreenSpaces: ArrayList<String>,
+    val badges: ArrayList<String>,
+    val favorites: ArrayList<String>,
+    val checkins: MutableMap<String, ArrayList<String>>
+)
