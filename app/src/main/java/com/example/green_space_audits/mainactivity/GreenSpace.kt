181c29bc3eec91628f7dfb27fe023b4d06d6750a
Jessica Kerry <jessica.d.kerry@gmail.com>
2019-11-28 16:34:52 -0500

    added basic display green space activity

--- app/src/main/java/com/example/green_space_audits/mainactivity/GreenSpace.kt
+++ app/src/main/java/com/example/green_space_audits/mainactivity/GreenSpace.kt
@@ -1,20 +1,21 @@
 package com.example.green_space_audits.mainactivity
 
-data class GreenSpace (val gsName: String,
-                       val gsCreator: String,
-                       val gsLat: Float,
-                       val gsLong: Float,
-                       val gsAcres: Float,
-                       val gsQuality: Quality,
-                       val gsType: Recreation,
-                       val gsComments: MutableMap<String, Comment>,
-                       val isQuiet: Boolean,
-                       val isNearHazards: Boolean)
+data class GreenSpace (val gsName: String = "",
+                       val gsCreator: String = "",
+                       val gsLat: Float = 0.toFloat(),
+                       val gsLong: Float = 0.toFloat(),
+                       val gsAcres: Float = 0.toFloat(),
+                       val gsQuality: Quality = Quality.LOW,
+                       val gsType: Recreation = Recreation.NATUREBASED,
+                       val gsComments: MutableMap<String, Comment> = mutableMapOf<String, Comment>(),
+                       val isQuiet: Boolean = false,
+                       val isNearHazards: Boolean = false)
 
-enum class Quality {
-    LOW, MED, HIGH
+enum class Quality(val displayStr: String) {
+    LOW("Low"), MED("Medium"), HIGH("High")
+
 }
 
-enum class Recreation {
-    PEOPLEPOWERED, NATUREBASED
+enum class Recreation(val displayStr: String) {
+    PEOPLEPOWERED("People-powered"), NATUREBASED("Nature-based")
 }
